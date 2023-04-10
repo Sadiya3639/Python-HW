8928303c879d06ca511fd4564c185703b93669cd
Sadiya Akter <127441365+Sadiya3639@users.noreply.github.com>
2023-04-11 00:45:01 +0600

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -15,7 +15,7 @@ class Profile:
 
 sadiya={"Sadiya Akter","Khulna","Khulna Mohila Polytechnic Institute","017********","19-02-2000"}
 aysha={"Aysha Siddika Rimpa","Bagerhat","Khulna Mohila Polytechnic Institute","013********","23-10-2003"}
-shanto={"Mostakim Mojammel Shanto","Khulna","hjdsfgsdvbshdfg","016********","16-01-1997"}
+shanto={"Mostakim Mojammel Shanto","Khulna","Tongi Govt. College, Gazipur","016********","16-01-1997"}
 sonia={"Sonia Akter","Khulna","Azam Khan Govt. Commerce College","019********","12-06-1997"}
 hira={"Hira Akter","Barishal","Khulna Mohila Polytechnic Institute","017********","18-07-2002"}
 nadiya={"Nadiya Khatun","Mongla","Khulna Govt. Girls College","019********","27-05-2001"}
